46255fff3f19a87b9ac9ddcb301cd2eedeab5579
Jacob Young <jacob@jry.io>
2021-11-07 15:25:30 -0500

    Add center dashed line
    Fixes positioning of sprites to comply with bevy's origin being in
    the center of the window. This is the default behavior and it seems
    easiest to cooperate with bevy.
    
    Adds a dashed line down the center of the window. Dashed line is just a
    batch of bevy SpriteBundles. As best as I can tell there is no way to
    render a dashed-line using the existing bevy APIs. Positioning of the
    dashed line is adaptive based on the window but uses pixels rather than
    relative distances.
    
    There is a slight misalignment I was not able to solve appropriately.
    The correct implementation involves:
    1. Defining a top/bottom "gutter" (space where no dashed line is
       rendered)
    2. Rendering a dashed line on top and one on bottom
    3. Computing the remaining space between lowest point of the top dash
       and the highest point of the bottom dash; then figuring out how many
       dashes can fit in between with equal spacing.
    
    This layout is quite similar to `flex` in the browser. Since Rust was
    written for Servo in Mozilla it only figures that we find ourselves at
    this point again. Déjà vu.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
 use bevy::prelude::*;
 use bevy::render::pass::ClearColor;
 
-// Entities
+/*
+ * Entities
+ */
+
 enum PlayerType {
-    TopPlayer,
-    BottomPlayer,
+    LeftPlayer,
+    RightPlayer,
 }
 struct Paddle {
     player_type: PlayerType,
 }
 
-// Components
+/*
+ * Components
+ */
 
+// Positions are percentage based
 struct Position {
     x: f32,
     y: f32,
 }
 
+// Vectors components are described as scalars between [0,1]
+// with two decimal places of precision
 struct Vector {
     x: f32,
     y: f32,
@@ -24,30 +32,46 @@ struct Vector {
 
 struct Ball;
 
-fn initialize_player(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
-    println!("initialize_player called");
+const LEFT_PLAYER_ORIGIN: Position = Position { x: -25.0, y: 0.0 };
+const RIGHT_PLAYER_ORIGIN: Position = Position { x: 25.0, y: 0.0 };
+const BALL_ORIGIN: Position = Position { x: 0.0, y: 0.0 };
+const PADDLE_SIZE: [f32; 2] = [6.0, 36.0];
+const DASH_WIDTH: f32 = 1.0;
+const DASH_HEIGHT: f32 = 8.0;
+const DASH_SIZE: [f32; 2] = [DASH_WIDTH, DASH_HEIGHT];
+const DASH_PADDING: f32 = 20.0;
+
+// The origin (0,0) of bevy's coordinate system is in the center of the screen
+fn startup_system(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    window: Res<WindowDescriptor>,
+) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(UiCameraBundle::default());
+    // Left Player
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
-            sprite: Sprite::new(Vec2::new(20.0, 100.0)),
+            sprite: Sprite::new(Vec2::from(PADDLE_SIZE)),
             ..Default::default()
         })
         .insert(Paddle {
-            player_type: PlayerType::TopPlayer,
+            player_type: PlayerType::LeftPlayer,
         })
-        .insert(Position { x: 20.0, y: 50.0 });
+        .insert(LEFT_PLAYER_ORIGIN);
+    // Right Player
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
-            sprite: Sprite::new(Vec2::new(20.0, 100.0)),
+            sprite: Sprite::new(Vec2::from(PADDLE_SIZE)),
             ..Default::default()
         })
         .insert(Paddle {
-            player_type: PlayerType::BottomPlayer,
+            player_type: PlayerType::RightPlayer,
         })
-        .insert(Position { x: 50.0, y: 50.0 });
+        .insert(RIGHT_PLAYER_ORIGIN);
+    // Ball
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
@@ -55,32 +79,52 @@ fn initialize_player(mut commands: Commands, mut materials: ResMut<Assets<ColorM
             ..Default::default()
         })
         .insert(Ball)
-        .insert(Position { x: 30.0, y: 50.0 });
+        .insert(BALL_ORIGIN);
+
+    // Compute how many dashes can fit evenly vertically in the window with DASH_PADDING inbetween
+    let window_top = (window.height / 2.0).abs();
+    let dash_material = materials.add(Color::rgb(1.0, 1.0, 1.0).into());
+    let dash_sprite = Sprite::new(Vec2::from(DASH_SIZE));
+    let dash_count = ((window.height) / (DASH_HEIGHT + DASH_PADDING)).floor() as u16;
+    let mut dashes = vec![];
+    for i in 0..=dash_count {
+        // let padding = if i == 0 { 0.0 } else { DASH_PADDING };
+        let y = window_top - (i as f32 * (DASH_PADDING + DASH_HEIGHT));
+        dashes.push(SpriteBundle {
+            material: dash_material.clone(),
+            sprite: dash_sprite.clone(),
+            transform: Transform::from_translation(Vec3::new(0.0, y, 0.0)),
+            ..Default::default()
+        });
+    }
+
+    commands.spawn_batch(dashes);
 }
 
 // Convert relative position to absolute
 fn render_system(mut query: Query<(&Position, &mut Transform)>, window: Res<WindowDescriptor>) {
     for (position, mut transform) in query.iter_mut() {
         let translation = &mut transform.translation;
-        translation.x = (position.x - 50.0) / 100.0 * window.width;
-        translation.y = (position.y - 50.0) / 100.0 * window.height;
+        translation.x = position.x / 100.0 * window.width;
+        translation.y = position.y / 100.0 * window.height;
     }
 }
 
 // 1. query for paddles, get components, mutate position
 fn main() {
     App::build()
+        // Clear Color is the background color
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .insert_resource(WindowDescriptor {
             title: "Bevy Pong".to_string(),
             width: 1000.0,
-            height: 1000.0,
+            height: 800.0,
             resizable: false,
             vsync: true,
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
-        .add_startup_system(initialize_player.system())
+        .add_startup_system(startup_system.system())
         .add_system(render_system.system())
         .run();
 }
