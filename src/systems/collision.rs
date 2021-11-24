a57b274ccb6aae44e0c962f73aac71a170b50812
Jacob Young <jacob@jry.io>
2021-11-24 01:07:17 -0500

    Cleaner rewrite of collision system

--- src/systems/collision.rs
+++ src/systems/collision.rs
@@ -1,161 +1,39 @@
-use crate::{Ball, Player, PlayerType, Velocity, WallSide};
+use crate::{Ball, Collidable, Player, PlayerType, Velocity, WallSide};
 use bevy::{prelude::*, sprite};
 
 type Collision = sprite::collide_aabb::Collision;
 
-// TODO: Bevy provides a mechanism to create sub-systems. I imagine we would group together
-// multiple different collision systems together in order to compose everything.
+#[allow(clippy::type_complexity)]
 pub fn collision_system(
     mut commands: Commands,
     mut ball_query: Query<(Entity, &Transform, &Sprite, &mut Velocity, &Ball)>,
-    player_query: Query<(&Transform, &Sprite, &Player)>,
-    wall_query: Query<(&Transform, &Sprite, &WallSide)>,
+    collidables_query: Query<(&Collidable, Option<&WallSide>, &Transform, &Sprite)>,
 ) {
-    if let Ok((entity, ball_transform, ball_sprite, mut ball_velocity, _)) = ball_query.single_mut()
+    if let Ok((ball_entity, ball_transform, ball_sprite, mut ball_velocity, _)) =
+        ball_query.single_mut()
     {
-        for (wall_transform, wall_size, wall_side) in wall_query.iter() {
-            if let Some(side) = wall_collision(
-                ball_transform,
-                ball_sprite,
-                wall_transform,
-                wall_size,
-                wall_side,
-            ) {
-                // https://bevy-cheatbook.github.io/programming/commands.html
-                commands.entity(entity).insert(side);
-                break;
-            }
-        }
-
-        for (player_transform, player_size, player_type) in player_query.iter() {
-            paddle_collision(
-                ball_transform,
-                ball_sprite,
-                &mut ball_velocity,
-                player_transform,
-                player_size,
-                player_type,
-            );
-        }
-    }
-}
-
-fn wall_collision(
-    ball_transform: &Transform,
-    ball_sprite: &Sprite,
-    wall_transform: &Transform,
-    wall_sprite: &Sprite,
-    wall_side: &WallSide,
-) -> Option<WallSide> {
-    let ball_pos = ball_transform.translation;
-    let ball_size = ball_sprite.size; //Vec2::from((ball_size.width, ball_size.height));
-    let wall_pos = wall_transform.translation;
-    let wall_size = wall_sprite.size; // Vec2::from((wall_.size.x, wall_size.size.y));
-
-    // let collision = sprite::collide_aabb::collide(ball_pos, ball_size, wall_pos, wall_size);
-    match (
-        wall_side,
-        sprite::collide_aabb::collide(ball_pos, ball_size, wall_pos, wall_size),
-    ) {
-        (WallSide::Left, Some(_)) => Some(WallSide::Left),
-        (WallSide::Right, Some(_)) => Some(WallSide::Right),
-        (_, None) => None,
-    }
-}
-
-fn paddle_collision(
-    ball_transform: &Transform,
-    ball_sprite: &Sprite,
-    ball_velocity: &mut Velocity,
-    player_transform: &Transform,
-    player_sprite: &Sprite,
-    player_type: &Player,
-) {
-    let ball_pos = ball_transform.translation;
-    let ball_size = ball_sprite.size; // Vec2::from((ball_size.width, ball_size.height));
-    let player_pos = player_transform.translation;
-    let player_size = player_sprite.size; // Vec2::from((player_size.width, player_size.height));
-    if let Some(collision) =
-        sprite::collide_aabb::collide(ball_pos, ball_size, player_pos, player_size)
-    {
-        match (player_type, collision) {
-            // Right Player
-            (
-                Player {
-                    player_type: PlayerType::Right,
-                },
-                Collision::Left,
-            ) => {
-                println!("Ball collided with the right paddle on the left size");
-                ball_velocity.0.x *= -1.0;
-                ball_velocity.0.y *= -1.0;
-            }
-            (
-                Player {
-                    player_type: PlayerType::Right,
-                },
-                Collision::Top,
-            ) => {
-                println!("Ball collided with the right paddle on the top size");
-                ball_velocity.0.x *= -1.0;
-                ball_velocity.0.y *= -1.0;
-            }
-            (
-                Player {
-                    player_type: PlayerType::Right,
-                },
-                Collision::Bottom,
-            ) => {
-                println!("Ball collided with the right paddle on the bottom size");
-                ball_velocity.0.x *= -1.0;
-                ball_velocity.0.y *= -1.0;
-            }
-            (
-                Player {
-                    player_type: PlayerType::Right,
-                },
-                _,
-            ) => {
-                println!("Ball collided with the right paddle on top, right, or bottom");
-            }
-            // Left Player
-            (
-                Player {
-                    player_type: PlayerType::Left,
-                },
-                Collision::Right,
-            ) => {
-                println!("Ball collided with the left paddle on the right side");
-                ball_velocity.0.x *= -1.0;
-                ball_velocity.0.y *= -1.0;
-            }
-            (
-                Player {
-                    player_type: PlayerType::Left,
-                },
-                Collision::Top,
-            ) => {
-                println!("Ball collided with the left paddle on the top side");
-                ball_velocity.0.x *= -1.0;
-                ball_velocity.0.y *= -1.0;
-            }
-            (
-                Player {
-                    player_type: PlayerType::Left,
-                },
-                Collision::Bottom,
-            ) => {
-                println!("Ball collided with the left paddle on the bottom side");
-                ball_velocity.0.x *= -1.0;
-                ball_velocity.0.y *= -1.0;
-            }
-            (
-                Player {
-                    player_type: PlayerType::Left,
-                },
-                _,
-            ) => {
-                println!("Ball collided with the left paddle on the top, left, or bottom")
+        for (collide_type, wallside, collide_transform, collide_sprite) in collidables_query.iter()
+        {
+            let ball_pos = ball_transform.translation;
+            let ball_size = ball_sprite.size;
+            let collide_pos = collide_transform.translation;
+            let collide_size = collide_sprite.size;
+            if let Some(collision) =
+                sprite::collide_aabb::collide(ball_pos, ball_size, collide_pos, collide_size)
+            {
+                match (collide_type, collision, wallside) {
+                    (Collidable::Reflect, _, _) => {
+                        ball_velocity.0.x *= -1.0;
+                        ball_velocity.0.y *= -1.0;
+                    }
+                    (Collidable::End, _, Some(WallSide::Left)) => {
+                        commands.entity(ball_entity).insert(WallSide::Left);
+                    }
+                    (Collidable::End, _, Some(WallSide::Right)) => {
+                        commands.entity(ball_entity).insert(WallSide::Left);
+                    }
+                    (_, _, None) => (),
+                }
             }
         }
     }
